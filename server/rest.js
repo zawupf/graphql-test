693c7efc9f85cf7fe2e4d66f636ef3216849ce19
Arnold Filip <arnold.filip@gmail.com>
2018-01-27 15:29:47 +0100

    Add mutations

--- server/rest.js
+++ server/rest.js
@@ -1,7 +1,52 @@
 const fetch = require('isomorphic-unfetch')
+
+const url = (...args) => (
+  [`http://localhost:3000`, ...args].join('/')
+)
+
 const get = (...args) => {
-  const url = [`http://localhost:3000`, ...args].join('/')
-  return fetch(url).then(res => res.json())
+  return fetch(url(...args)).then(res => res.json())
 }
 
-module.exports = { get }
+const post = (...args) => {
+  const _url = url(...args.slice(0, args.length - 1))
+  const body = JSON.stringify(args[args.length - 1])
+  return fetch(_url, {
+    method: 'POST',
+    body,
+    // mode: 'cors',
+    // cache: 'no-cache',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+  })
+}
+
+const patch = (...args) => {
+  const _url = url(...args.slice(0, args.length - 1))
+  const body = JSON.stringify(args[args.length - 1])
+  return fetch(_url, {
+    method: 'PATCH',
+    body,
+    // mode: 'cors',
+    // cache: 'no-cache',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+  })
+}
+
+const _delete = (...args) => {
+  return fetch(url(...args), {
+    method: 'DELETE',
+    // mode: 'cors',
+    // cache: 'no-cache',
+    headers: {
+      'Accept': 'application/json',
+    },
+  })
+}
+
+module.exports = { get, post, patch, _delete }
--- server/schema.js
+++ server/schema.js
@@ -41,6 +41,50 @@ const RootQueryType = new GraphQLObjectType({
   }
 })
 
+// Mutations
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addCustomer: {
+      type: CustomerType,
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        email: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
+      },
+      resolve(parentValue, args) {
+        const { name, email, age } = args
+        return rest.post('customers', { name, email, age })
+          .then(res => res.json())
+      }
+    },
+    editCustomer: {
+      type: CustomerType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        age: { type: GraphQLInt },
+      },
+      resolve(parentValue, args) {
+        return rest.patch('customers', args.id, args)
+          .then(res => res.json())
+      }
+    },
+    deleteCustomer: {
+      type: CustomerType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parentValue, args) {
+        return rest._delete('customers', args.id)
+          .then(res => res.json())
+      }
+    },
+  }
+})
+
 module.exports = new GraphQLSchema({
   query: RootQueryType,
+  mutation,
 })
